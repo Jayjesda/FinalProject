fe138e2fa363be12ab853389102f5cf79b3c0e4c
Jayjesda <87358825+Jayjesda@users.noreply.github.com>
2021-11-23 16:33:30 +0700

    Add files via upload

---
+++ admin/includes/sidebar.php
@@ -0,0 +1,30 @@
+  <div id="layoutSidenav_nav">
+                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                    <div class="sb-sidenav-menu">
+                        <div class="nav">
+                            <div class="sb-sidenav-menu-heading">รายการ</div>
+                            <a class="nav-link" href="dashboard.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                                หน้าแรก
+                            </a>
+                            
+                 
+                            <a class="nav-link" href="manage-users.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-users"></i></div>
+                                จัดการผู้ใช้งานระบบ
+                            </a>
+
+ <a class="nav-link" href="bwdates-report-ds.php">
+                                <div class="sb-nav-link-icon"><i class="fa fa-calendar"></i></div>
+                                รายงานประจำวัน
+                            </a>
+
+                            <a class="nav-link" href="logout.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-sign-out-alt"></i></div>
+                                ออกจากระบบ
+                            </a>
+                        </div>
+                    </div>
+                
+                </nav>
+            </div>
